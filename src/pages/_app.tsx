58bea2571d117e1836b13b6810e2444fcd9cb9b2
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-06-03 15:12:01 -0400

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import type { ReactElement, ReactNode } from "react";
+import type { ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import RootLayout from "./layout";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+  getLayout?: () => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
