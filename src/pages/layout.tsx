1b700627fac6f40284631e8d6b82f140ac3874ac
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-06-03 14:27:15 -0400

    Add favicon

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import Head from "next/head";
+
 import "../app/globals.css";
 
 export default function RootLayout({
@@ -5,5 +8,36 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <React.Fragment>
+      <Head>
+        <title>The Crystal Chapel</title>
+        <meta
+          name="description"
+          content="Test website for The Crystal Chapel"
+        />
+
+        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+      {children}
+    </React.Fragment>
+  );
 }
