6c23b073657744610abd00e2ef5f58d1bb9097e6
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-06-05 23:50:53 -0400

    Add collection view route

--- src/containers/home/components/Capsules/components/CapsulesSection/CapsulesSection.tsx
+++ src/containers/home/components/Capsules/components/CapsulesSection/CapsulesSection.tsx
@@ -1,12 +1,17 @@
 import { CAPSULES } from "@/modules/capsules/constants/CAPSULES";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function CapsulesSection() {
   return (
     <section className="capsules-section gap-y-12 gap-x-6">
       {CAPSULES.map((c, i) => (
-        <div key={i} className="w-full flex-col flex">
+        <Link
+          className="w-full flex-col flex transition-all duration-300 hover:scale-105 cursor-pointer"
+          key={i}
+          href={`/view/${i}`}
+        >
           <Image
             width={400}
             height={200}
@@ -15,12 +20,11 @@ export default function CapsulesSection() {
             loading="lazy"
             style={{ objectFit: "cover" }}
           />
-
           <div className="text-xl mt-2">
             <p className="font-InterSemiBold">Capsule</p>
             <h1 className="uppercase font-InterExtraBold">{c.title}</h1>
           </div>
-        </div>
+        </Link>
       ))}
     </section>
   );
