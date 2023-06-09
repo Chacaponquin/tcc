566b13ef932fbcffca7cbc37fce683f1f3bdc520
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-06-09 14:31:18 -0400

    Delete capsules constants

--- src/modules/capsules/constants/CAPSULES.ts
+++
@@ -1,5 +0,0 @@
-import { Capsule } from "../interfaces/capsule.interface";
-
-export const CAPSULES: Array<Capsule> = new Array(5)
-  .fill(0)
-  .map(() => ({ title: "Capsule 1", image: "/images/test.jpg" }));
